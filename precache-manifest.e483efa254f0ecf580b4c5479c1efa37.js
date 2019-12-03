b7ef1ff0185e42e8569b33388123e2568d18db44
Phuong <phuongna1908@gmail.com>
2019-12-03 23:51:20 +0700

    +

---
+++ precache-manifest.e483efa254f0ecf580b4c5479c1efa37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ceff0b4d62497622d346e8681b1eb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "932ab83fb3e878277f52",
+    "url": "/static/css/main.57045bed.chunk.css"
+  },
+  {
+    "revision": "99821fc8bc9946e12cc2",
+    "url": "/static/js/2.2aa3bbe7.chunk.js"
+  },
+  {
+    "revision": "932ab83fb3e878277f52",
+    "url": "/static/js/main.bdfa389e.chunk.js"
+  },
+  {
+    "revision": "189e974464492cd9afc8",
+    "url": "/static/js/runtime-main.bb360eb3.js"
+  }
+]);
